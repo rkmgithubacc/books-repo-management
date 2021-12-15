7c107849bd17056a7ecc28e4b30c24f93f0bf6e7
RoshanKM <roshan.techacc88@gmail.com>
2021-12-15 16:20:56 +0530

    First commit main branch

---
+++ src/main/java/com/rkmgithubacc/booksrepomanagement/BooksRepoManagementApplication.java
@@ -0,0 +1,13 @@
+package com.rkmgithubacc.booksrepomanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksRepoManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksRepoManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rkmgithubacc/booksrepomanagement/BooksRepoManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkmgithubacc.booksrepomanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksRepoManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
